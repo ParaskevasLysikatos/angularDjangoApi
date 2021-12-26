b3a8788fdd00ce401957f70c741e27f3350396e9
DESKTOP-AQ46HRP\Par <vaslysalex@hotmail.gr>
2021-12-26 19:05:07 +0200

    origin fix8

--- angularEmployeesDevices/settings.py
+++ angularEmployeesDevices/settings.py
@@ -48,7 +48,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
-    #'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.http.ConditionalGetMiddleware'
